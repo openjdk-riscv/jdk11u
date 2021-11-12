14c21bbb6d28f5d89861cb94c796a2bd8cc88920
zifeihan <caogui@iscas.ac.cn>
2021-11-12 10:42:46 +0800

    Fix the get_native_u8, put_native_u8 function cannot get the correct result.

--- src/hotspot/cpu/riscv32/bytes_riscv32.hpp
+++ src/hotspot/cpu/riscv32/bytes_riscv32.hpp
@@ -64,26 +64,24 @@ class Bytes: AllStatic {
   }
 
   static inline u8 get_native_u8(address p) {
-    switch (intptr_t(p) & 7) {
-      case 0:
-        return ((u8)((u4*)(p + 4)) << 32) |
-               ((u8)((u4*)p));
-      case 2:
-        return ((u8)(((u2*)(p + 4))[1]) << 48) |
-               ((u8)(((u2*)(p + 4))[0]) << 32) |
-               ((u8)(((u2*)p)[1]) << 16) |
-               ((u8)(((u2*)p)[0]));
-      default:
-        return ((u8)((p + 4)[3]) << 56) |
-               ((u8)((p + 4)[2]) << 48) |
-               ((u8)((p + 4)[1]) << 40) |
-               ((u8)((p + 4)[0]) << 32) |
-               ((u8)(p[3]) << 24) |
-               ((u8)(p[2]) << 16) |
-               ((u8)(p[1]) <<  8) |
-                (u8)(p[0]);
+      switch (intptr_t(p) & 7) {
+        case 0:  return *(u8*)p;
+        case 4:  return u8(((u4*)p)[0]) |
+                        u8(((u4*)p)[1]) << 32;
+        case 2:  return u8(((u2*)p)[0])       |
+                        u8(((u2*)p)[1]) << 16 |
+                        u8(((u2*)p)[2]) << 32 |
+                        u8(((u2*)p)[3]) << 48;
+        default: return u8(p[0])       |
+                        u8(p[1]) <<  8 |
+                        u8(p[2]) << 16 |
+                        u8(p[3]) << 24 |
+                        u8(p[4]) << 32 |
+                        u8(p[5]) << 40 |
+                        u8(p[6]) << 48 |
+                        u8(p[7]) << 56;
+      }
     }
-  }
 
   static inline void put_native_u2(address p, u2 x) {
     if ((intptr_t(p) & 1) == 0) {
@@ -113,29 +111,27 @@ class Bytes: AllStatic {
   }
 
   static inline void put_native_u8(address p, u8 x) {
-    switch (intptr_t(p) & 3) {
-      case 0:
-        *(u4*)(p + 4) = x >> 32;
-        *(u4*)p = x;
-        break;
-      case 2:
-        ((u2*)(p + 4))[1] = x >> 48;
-        ((u2*)(p + 4))[0] = x >> 32;
-        ((u2*)p)[1] = x >> 16;
-        ((u2*)p)[0] = x;
-        break;
-      default:
-        ((u1*)(p + 4))[3] = x >> 56;
-        ((u1*)(p + 4))[2] = x >> 48;
-        ((u1*)(p + 4))[1] = x >> 40;
-        ((u1*)(p + 4))[0] = x >> 32;
-        ((u1*)p)[3] = x >> 24;
-        ((u1*)p)[2] = x >> 16;
-        ((u1*)p)[1] = x >>  8;
-        ((u1*)p)[0] = x;
-        break;
+      switch (intptr_t(p) & 7) {
+        case 0:  *(u8*)p = x;
+                 break;
+        case 4:  ((u4*)p)[0] = x;
+                 ((u4*)p)[1] = x >> 32;
+                 break;
+        case 2:  ((u2*)p)[0] = x;
+                 ((u2*)p)[1] = x >> 16;
+                 ((u2*)p)[2] = x >> 32;
+                 ((u2*)p)[3] = x >> 48;
+                 break;
+        default: ((u1*)p)[0] = x;
+                 ((u1*)p)[1] = x >>  8;
+                 ((u1*)p)[2] = x >> 16;
+                 ((u1*)p)[3] = x >> 24;
+                 ((u1*)p)[4] = x >> 32;
+                 ((u1*)p)[5] = x >> 40;
+                 ((u1*)p)[6] = x >> 48;
+                 ((u1*)p)[7] = x >> 56;
+      }
     }
-  }
 
   // Efficient reading and writing of unaligned unsigned data in Java byte ordering (i.e. big-endian ordering)
   static inline u2   get_Java_u2(address p) { return swap_u2(get_native_u2(p)); }
